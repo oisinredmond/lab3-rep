e766b22270a72fd2494a61ad4b98a54eba5f600b
Oisin Redmond <oisinredmond@gmail.com>
2017-10-04 14:42:12 +0000

    Added various routes to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from flask_mysqldb import MySQL
+
 mysql = MySQL()
 app = Flask(__name__)
 # My SQL Instance configurations 
@@ -19,6 +20,29 @@ def hello(): # Name of the method
     cur.execute('''SELECT * FROM students''') # execute an SQL statment
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
     return str(rv)      #Return the data in a string format
-if __name__ == "__main__":
-        app.run(host='0.0.0.0', port='5000') #Run the flask app at port 5000
 
+@app.route("/newrec")
+def addstudent():
+    cur = mysql.connection.cursor()
+    cur.execute('''INSERT INTO students (studentName, email) values ("third student","thirdstudent@mydit.ie")''')
+    return "added a third student"
+
+@app.route("/update")
+def update():
+    cur = mysql.connection.cursor()
+    cur.execute('''UPDATE students SET studentName = "John Doe" WHERE studentName = "first student"''')
+    return "changed third student's name"
+
+@app.route("/delete")
+def delete():
+    cur = mysql.connection.cursor()
+    cur.execute('''DELETE FROM students WHERE studentName = "third student"''')
+    return "deleted the third student"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port='5000')
